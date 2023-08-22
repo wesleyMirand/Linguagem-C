6b0221edba66d137b32dfaef3f41a62418b32769
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 00:09:26 -0300

    Create voidmain.c

---
+++ voidmain.c
@@ -0,0 +1,18 @@
+//agora com void main, um vazio, funcao programa que nao retorna nada
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//funcao principal do programa
+void main() {
+    //imprime na tela
+    printf("Oi Mundo");
+
+    //Pausa o programa após executar
+    system("pause");
+
+    //Retorna o valor 0
+    return 0;
+}
+
+//na apple da problema usar void man que signifca vazio ai nao precisa do return 0. 
