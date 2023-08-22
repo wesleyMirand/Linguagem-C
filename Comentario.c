388e8b013158da91281248d5868e52a7e315702e
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 00:09:00 -0300

    Create Comentario.c

---
+++ Comentario.c
@@ -0,0 +1,16 @@
+e agora comentario para ter organizacao
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//funcao principal do programa
+int main() {
+    //imprime na tela
+    printf("Oi Mundo");
+
+    //Pausa o programa após executar
+    system("pause");
+
+    //Retorna o valor 0
+    return 0;
+}
