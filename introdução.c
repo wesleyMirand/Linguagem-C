3b166d9362537566960462e135f194935cbe5116
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 00:02:01 -0300

    Create introdução.c

---
+++ introdução.c
@@ -0,0 +1,11 @@
+//Bibliotecas são cojuntos de funcoes que vao fazer as variadas coisas, imprimir dados, ler dados, matematicas, a biblioteca padrão é a "stdio", padrão de entrada e saida extend alt. vamos incluir isso no arquivo intao: vai ser uma funcao inteira, vai retorna um numero 0 inteiro
+//Entre parenteses () <- se coloca parametros para executar 
+//E entre {} vai fica tudo que pertence a funcao main
+//a rodar o arquivo vai gerar um ".o" que é gerado sempre que compila junto com uma linguagem proxima da maquina de configuracao de program junto com o arquivo executavel
+
+
+#include <stdioh.h>
+
+int main() {
+    return 0;
+}
