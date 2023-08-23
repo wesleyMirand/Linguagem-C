09d34d69ae289589491a797d09a7d31c1f61dafc
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:11:07 -0300

    Create divisao.c

---
+++ divisao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1 = 25, num2= 12, resultado;
+    resultado =  num1 * num2;//divisao de numeros inteiro e flutuantes tambem
+
+    printf("%i", resultado);
+
+    return 0;
+}
