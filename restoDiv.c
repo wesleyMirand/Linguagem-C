1e9b925fbc1622c56e1e8edab2d92bbaa27ad071
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:13:44 -0300

    Create restoDiv.c

---
+++ restoDiv.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num1 = 25, num2= 2, resultado;//dividir 25 por 2
+    resultado = num1 % num2;// resto da divisao
+    printf("%i", resultado);
+
+    return 0;
+}
