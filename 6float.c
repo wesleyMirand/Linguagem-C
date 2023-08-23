ebbccb570c864d43773dcebeec60abd5e52e16ce
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:16:54 -0300

    Create 6float.c

---
+++ 6float.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num1 = 2, num2 = 5, num3 = 3, resultado;
+    resultado = (num1 + num2) * num3; //vai resolver primeiro que estao entre pareteses ( ) 
+    
+    printf("%i\n", resultado);
+    
+    return 0;
+}
