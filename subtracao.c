febd23c43069d55bd0a74012dada86adc1426876
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:10:42 -0300

    Create subtracao.c

---
+++ subtracao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num2 = 10, resultado;
+    resultado =  -num2; //fazendo subtracao num1 - num2 e tambem troca de sinal  -num2
+
+    printf("%i", resultado);
+
+    return 0;
+}
