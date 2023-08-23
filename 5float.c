86002ba46e4d49cece192dae69c80c13649bc14b
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:15:36 -0300

    Create 5float.c

---
+++ 5float.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num1 = 24, num2 = 5, resultado;
+    //resultado = num1 % num2;
+    printf("%i", ++num1);//Desse jeito com o ++ pra tras ele vai comecar primeiro daqui > pra la <, primeiro incrementa e depois realiza a operacao
+
+    return 0;
+}
