c5fb9a6cca94cfc670396ec03281eed8bef3ba9f
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:22:47 -0300

    Create dadosrelacionais.c

---
+++ dadosrelacionais.c
@@ -0,0 +1,20 @@
+#include <stdio.h> //saida de dados pode ocorrer de varias formas, as mais comuns sao atraves do monitor ou impressora, aqui vai ser so na tela, usamos essa funcao na biblioteca stdio.h, utiliza para imprimir na tela valores
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+    /*int valorLido;
+    scanf("%i", &valorLido);
+    printf("%i\n", valorLido); //vai mostrar o valor digitado */
+
+    /*char valorLido;
+    scanf("%c", &valorLido);
+    printf("%c\n", valorLido); //leitura caracter */
+
+    char valorLido;
+    valorLido = getch(); //vai ler oque digita, funcao especial para windows
+    printf("Valor lido: %c\n", valorLido); //outra forma para fazer leitura de char, existem mais formas de leitura mais ess e so de letra
+
+    return 0;
+}
