173644bbbc423692d9ba65026b8290e6eb8e3e45
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:15:09 -0300

    Create 4float.c

---
+++ 4float.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num1 = 24, num2 = 5, resultado;
+    //resultado = num1 % num2;
+    printf("%i", num1++);//1 a mnais coloquei dentro do pritnf, primeiro resolve a "%i" e depoois incrementa 
+
+    return 0;
+}
