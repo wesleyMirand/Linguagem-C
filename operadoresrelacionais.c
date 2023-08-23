ee8079aad4463fcceb7b77bd8e2795a772d0f8fb
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:20:27 -0300

    Create operadoresrelacionais.c

---
+++ operadoresrelacionais.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    ////operadores relacionais responsaveis pelas comparacoes de expressoe
+    /* float num1 = 2, num2 = 5, num = 3;
+    > maior
+    >= maior igual 
+    <menor
+    <= menor igual
+    ==igualdades
+    !=Diferente */
+    
+    
+
+    return 0;
+}
