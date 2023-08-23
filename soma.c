9b9eee579b9d0ce41c83082b99f0a391a868b03d
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:09:34 -0300

    Create soma.c

---
+++ soma.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h> //bliblioteca que usa system e etc...
+
+int main()
+{
+    int num1, num2, soma; //primeira var criada, tipo de dado
+    printf("Informe num1: ");
+    scanf("%i", %num1);
+
+    printf("Informe num2: ");
+    scanf("%i", %num2);// nos numeros inteiro tanto posso usar o %i como o %d
+
+    soma = num1 * num2;
+
+    printf("Resultado: %i\n", soma); //mostrar na tela a var
+
+    system("pause");
+    return 0;
+}
