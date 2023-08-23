fdd6327c5a315cb0d4c41d1ecb7391d25f7f1729
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:12:27 -0300

    Create 2float.c

---
+++ 2float.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num1 = 25, num2= 12, resultado;
+    float resultado;// o ideal e usar float em tudo que vai float
+    resultado =  num1 / num2;//divisao de numeros inteiro e flutuantes tambem e multiplicacao tbm
+
+    printf("%f", resultado);
+
+    return 0;
+}
