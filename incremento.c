1deb9c65ad724db1f4d3eb59ffcd49c099712fef
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-22 21:14:13 -0300

    Create incremento.c

---
+++ incremento.c
@@ -0,0 +1,12 @@
+ #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float num1 = 24, num2 = 5, resultado;
+    //resultado = num1 % num2;
+    num1++; //valor de incremento n1 = n1+ 1, adiciona uma unidade;
+    printf("%i", num1);
+
+    return 0;
+}
