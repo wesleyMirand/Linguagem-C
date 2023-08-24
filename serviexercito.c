8599604b6731c6bf1b392a79d7a3280072b33674
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-23 23:57:37 -0300

    Create serviexercito.c

---
+++ serviexercito.c
@@ -0,0 +1,16 @@
+
+include<stdio.h>
+include<stdlib.h>
+include<conio>
+int main() {
+    int idade;
+    printf("QUantos anos voce tem para quer servi o exercito maromba?");
+    scanf("%i", &idade);
+    if (idade>=18) {
+        printf("convocado vem pro exercito!!!!\n");
+     }
+     else {
+        printf("infelizmente nao da!\n");
+     }
+ return 0;
+} 
