a0c0fb015c4ae4f9cdab1b78bf5967b476e1dac9
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-24 00:00:19 -0300

    Create 6ExercitoElseElseIfElse.c

---
+++ 6ExercitoElseElseIfElse.c
@@ -0,0 +1,18 @@
+include<stdio.h>
+include<stdlib.h>
+include<conio>
+int main() {
+    int idade;
+    printf("QUantos anos voce tem para quer servi o exercito maromba?");
+    scanf("%i", &idade);
+    if (idade>=18 || idade<=25) {
+        printf("convocado vem pro exercito!!!!\n");
+     }
+     else if(idade>=25 && idade<=30) {
+        printf("convocado vem pro exercito!!!!\n");
+     }
+     else {
+        printf("infelizmente nao da!\n");
+     }
+ return 0;
+}
