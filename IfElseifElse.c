67a47b0d5029e45d2c99a8d0eae20ad70f46391a
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-24 13:39:31 -0300

    Create IfElseifElse.c

---
+++ IfElseifElse.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int valor;
+    printf("Digite a opcao:");
+        scanf("%i", &valor);
+
+
+        if(valor==10 ){
+            printf("oi");
+        }else if (valor == 5){
+            printf("eae");
+        }else{
+            printf("acabou tudo");
+        }
+
+    return 0;
+}
