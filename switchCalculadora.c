4a112e3581e4de99be295559132d32dcfcd7ac64
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-24 23:59:48 -0300

    Create switchCalculadora.c

---
+++ switchCalculadora.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float numero1, numero2, resultado;
+    char operacao;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%f", &numero1);
+
+    fflush(stdin);
+
+    printf("Digite a operacao: ");
+    scanf(" %c", &operacao);
+
+    printf("Digite o segundo numero: ");
+    scanf("%f", &numero2);
+    
+    switch(operacao) {
+           case'+';
+                  resultado = numero1 + numero2;
+           break
+           case'-';
+                  resultado = numero1 - numero2;
+           break
+           case'*';
+                  resultado = numero1 * numero2;
+           break
+           case'/';
+                   if(numero2 != 0) {
+            resultado = numero1 / numero2;
+         }else{
+            printf("Divisão por zero. ERRo \n!");
+            return 0;
+         }
+           break
+           default:
+            printf("operacao nao reconhecida");
+        return 0;
+    }
