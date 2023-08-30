9e98ca31b92fcd50bc17cd611a0edbc6332d36be
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-30 00:00:55 -0300

    Create 8DoWHile.c

---
+++ 8DoWHile.c
@@ -0,0 +1,16 @@
+//ja assim vai executar ola mundo 10 vezes
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 1;
+    //sintax do do while
+    do{
+            printf("Ola Mundo");
+    } while(i <= 10);
+/* vai entrar em loop infinito pq nao fizemos a conexao para trocar a var em algum momento, e ai tenque fazer com que var
+que estamos usando para fazer os teste tenha seu valor alterado*/
+    //printf("\n%i\n", i);
+   return 0;
+}
