2f3103d1dc5980fad4c59afa06c53e0f0d2353a5
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-30 00:03:26 -0300

    Create 7Incrementa.c

---
+++ 7Incrementa.c
@@ -0,0 +1,15 @@
+//icrementa até 1001
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 12;
+    do{
+            printf("%i\n", i);
+            i++;
+    } while(i <= 1000);
+    printf("\n%i\n", i);
+   return 0;
+}
