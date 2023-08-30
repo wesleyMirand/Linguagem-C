815af632e979f81c38c045b7207ad02ce9381bbf
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-30 00:00:01 -0300

    Create 6DoWhile.c

---
+++ 6DoWhile.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 1;
+    //sintax do do while
+    do{ //vai ser executada pelo menos uma vez sempre
+        BlocoDeExecucao
+    } while(TesteCondicional);
+    
+    printf("\n%i\n", i);
+   return 0;
+}
