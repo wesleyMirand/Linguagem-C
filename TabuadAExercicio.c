c09e3ffdc4e6db3efca40da95b7609fef60d9eb5
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-30 00:05:40 -0300

    Create TabuadAExercicio.c

---
+++ TabuadAExercicio.c
@@ -0,0 +1,15 @@
+//Tabuada normal: #include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int numeroLido, i;
+
+    printf("Informe o numero:");
+    scanf("%i", &numeroLido);
+
+    for(i = 1;  i<=10; i++){
+        printf("%i x %i = %i\n", numeroLido, i, numeroLido * i);
+    }
+    return 0;
+}
