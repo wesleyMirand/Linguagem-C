faa391cf34b89508587b0d839ead1d7cfa5ffd63
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-30 00:01:29 -0300

    Create 9DoWhile.c

---
+++ 9DoWhile.c
@@ -0,0 +1,15 @@
+//incrementa 1 e fica 13
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i = 12;
+    do{
+            printf("Ola Mundo");
+            i++;
+    } while(i <= 10);
+    printf("\n%i\n", i);
+   return 0;
+}
