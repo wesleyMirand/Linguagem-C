2d4d8620566a8289d3f6df8455266e0bcedeefff
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-08-31 23:57:32 -0300

    Create vetor4.c

---
+++ vetor4.c
@@ -0,0 +1,26 @@
+//fazer ler  via teclado, calcula media e mostrar
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define TAM 5
+
+int main()
+{
+    //tipo nomeVar[tam];
+    float vetNotas[TAM], media = 0; //valor da media tenque zero(nada) pq vai somar os valores dentro dela e dividi pela quantidade de valores
+    int i;
+    for( i = 0; i < TAM; i++) {
+            printf("Informe a nota: ");
+            scanf("%f", &vetNotas[i]);
+    }
+
+    for(i = 0; i < TAM; i++){
+        media += vetNotas[i];
+    }
+        media /=TAM;
+
+    printf("\nMedia = %.2f", media);
+
+    return 0;
+}
