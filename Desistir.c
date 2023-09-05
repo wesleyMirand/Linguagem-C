0a9e6924fffcb076c932b27d66f5cbb3682b57ab
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-09-05 15:42:31 -0300

    Create Desistir.c

---
+++ Desistir.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char Desistir, NaoDesistir;
+
+    printf("Eae Voce vai desistir?");
+    scanf("%c", &Desistir);
+
+    system("pause");
+
+    printf("Tem certeza que nao vai Desistir?");
+    scanf("%c", &NaoDesistir);
+    return 0;
+}
