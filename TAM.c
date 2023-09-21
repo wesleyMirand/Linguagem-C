0d7f44e56e00859021601e8d34668f3ef9829853
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-09-21 00:00:10 -0300

    Create TAM.c

---
+++ TAM.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define TAM 5
+
+int main() {
+  int vetNumeros[TAM], i, maior, indiceMaior;
+  
+  for(i= 0; i<TAM; i++) {
+      printf("Informe o numero: ");
+      scanf("%i", &vetNumero[i]);
+     }
+
+  indiceMaior = 0;
+  for(i = 1; i < TAM; i++) {
+      if(vetNumeros[i] > vetNumeros[indice Maior]) {
+           indiceMaior = i;
+   }
+}
+printf("\n\nMaior = %i, na posicao %i \n\n", vetNumeros [indiceMaior], indiceMaior);
+   return 0;
+ }
