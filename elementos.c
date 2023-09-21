15c542a0301c5f6ed5c8a43710bbdd22309efb90
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-09-20 23:59:21 -0300

    Create elementos.c

---
+++ elementos.c
@@ -0,0 +1,18 @@
+/*vetor de 5 posicoes
+qual maior elemento que é o 9 */
+#include<stdio.h>
+#include<stdlib.h>
+#define TAM 5
+
+int main() {
+  int vetNumeros[TAM], i, maior;
+  
+  for(i = 0; < TAM; i++) (
+      print("Informe o numero: ");
+      scanf("%i", &vetNumeros[i] > maior) {
+             maior = vetNumeros[i];
+       }
+      }
+   printf("\n\nMaior = %i\n\n", maior);
+   return 0;
+}
