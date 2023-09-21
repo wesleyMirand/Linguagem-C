57fc7ed0f01d27b40fd44acd67c7db01412d2a83
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-09-20 23:59:53 -0300

    Create maior.c

---
+++ maior.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define TAM 5
+
+int main() {
+  int vetNumeros[TAM], i, maior, indiceMaior;
+  
+  for(i = 0; < TAM; i++) (
+      print("Informe o numero: ");
+      scanf("%i", &vetNumeros[i] > maior) {
+             maior = vetNumeros[i];
+             indiceMaior = i;
+       }
+      }
+   printf("\n\nMaior = %i\n\n", maior, indiceMaior);
+   return 0;
+}
