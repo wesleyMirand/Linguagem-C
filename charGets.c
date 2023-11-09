c9d47301fe0bb31873c9db0c94650feb9b44fcdd
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-11-08 23:56:19 -0300

    Create charGets.c

---
+++ charGets.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h> 
+
+int main() {
+  char i = 0;
+   int i = 0;
+   int cout = 0;
+
+   printd("informe a palavra:") gets(palavra);
+   for ( i = 0 ; palavra[i] !=  '\o'; i + ;
+         count++;
+   }
+
+   printf("%d - ")
+
+   return 0;
+}
