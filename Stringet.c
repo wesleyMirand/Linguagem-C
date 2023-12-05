077f0919ac6275b399403e9159d9a5bed9f86634
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-04 22:07:40 -0300

    Create Stringet.c

---
+++ Stringet.c
@@ -0,0 +1,14 @@
+//lendo string com get, le tudo que é digitado no teclado até o espaço.
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+     char[51];
+     printf("informe o nome:");
+     //scanf("%s", nome);
+     gets(nome);
+
+     printf("mostre o nome digitado: %s\n", nome);
+
+  return 0;
+}
