4290e38f8234089b3b5f49873e145769f79d96f2
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-04 22:07:58 -0300

    Create StringPuts.c

---
+++ StringPuts.c
@@ -0,0 +1,17 @@
+
+//outra forma é o puts
+
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+   char[51];
+   printf("informe o nome:");
+   gets(nome);
+
+   printf("informe o nome: %s \n", nome);//valor pro usuario e o nome da string lida
+ 
+   puts(nome);
+
+  return 0;
+ }
