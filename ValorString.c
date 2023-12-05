e3a31979152d8effe087d69e84e365bfa7c521fb
WesleyLinux <56045146+wesleyMirand@users.noreply.github.com>
2023-12-04 22:05:37 -0300

    Create ValorString.c

---
+++ ValorString.c
@@ -0,0 +1,14 @@
+//valor de strings
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() 
+{
+  char nome_cliente[10] = "BBZINHO";
+  print("%s", nome_cliente);
+ 
+  return 0;
+}
+
+//cadeia de caracteres  percento "S" e o nome do vetor na cadeia.
